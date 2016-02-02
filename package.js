021d98b650746404185e030ee9782b7a5f9c6f1a
xavizalote <xavier.cazalot@gmail.com>
2016-02-02 16:38:47 +0100

    fix stack getter error
    Remove redirect uri

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xavizalote:telescope-slack',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'Automatically send Telescope posts as messages to connected Slack teams via the Add To Slack button',
   git: 'https://github.com/xavizalote/telescope-slack.git',
 
