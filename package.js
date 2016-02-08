864f415e5d49f41b290f0dd98c8ce7999199cf4a
xavizalote <xavier.cazalot@gmail.com>
2016-02-08 21:07:58 +0100

    postSubmitAsync callback to get post._id \o/

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xavizalote:telescope-slack',
-  version: '0.0.4',
+  version: '0.0.6',
   summary: 'Automatically send Telescope posts as messages to connected Slack teams via the Add To Slack button',
   git: 'https://github.com/xavizalote/telescope-slack.git',
 
