6becbe22f23a07790d30675e7714160ea26ee972
xavizalote <xavier.cazalot@gmail.com>
2016-06-21 12:57:57 +0200

    make the button dispatching messages and calling the request token method

--- lib/components/SlackButton.jsx
+++ lib/components/SlackButton.jsx
@@ -2,6 +2,31 @@ import React, { Component, PropTypes } from 'react';
 import { withRouter } from 'react-router'
 
 class SlackButton extends Component {
+
+  
+  componentDidMount() {
+    const { query } = this.props.location;
+    
+    // a code is present in the query params
+    if (!_.isEmpty(query) && query.code) {
+      // show the user that we are doing something
+      this.context.messages.flash('Connecting to Slack...', 'warning');
+      
+      // call the request token method
+      this.context.actions.call('slack.requestToken', query.code , (err, res) => {
+        // clear the loading message
+        this.context.messages.markAsSeen();
+        this.context.messages.clearSeen();
+
+        if(err) { // woops, something bad happened
+          this.context.messages.flash('WOOOPS!', 'danger');
+        } else { // woot we are all done!
+          this.context.messages.flash('DONE!', 'success');
+        }
+      });
+    }
+  }
+
   render() {
     const slackAppClientId = Telescope.settings.get('slackAppClientId');
 
@@ -12,12 +37,21 @@ class SlackButton extends Component {
             alt="Add to Slack" 
             style={{width: 139, height: 40}}
             src="https://platform.slack-edge.com/img/add_to_slack.png" 
-            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+            srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
           />
         </a>
       </div>
     ) : null;
   }
+};
+
+SlackButton.propTypes = {
+  location: React.PropTypes.object,
+};
+
+SlackButton.contextTypes = {
+  messages: React.PropTypes.object,
+  actions: React.PropTypes.object,
 };
 
 export default withRouter(SlackButton);
