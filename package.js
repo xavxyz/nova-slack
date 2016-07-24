261f5a7cd6bf22df526d989bd9fe11c39ec2fe91
xavcz <xavier.cazalot@gmail.com>
2016-07-24 12:58:29 +0200

    bump to v0.26.5

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'xavcz:nova-slack',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Automatically send Nova posts as messages to connected Slack teams via the Add To Slack button',
   git: 'https://github.com/xavcz/nova-slack.git',
   documentation: 'README.md'
@@ -13,11 +13,11 @@ Package.onUse(function(api) {
   api.versionsFrom(['METEOR@1.0']);
 
   api.use([
-    'nova:core@0.26.3-nova',
-    'nova:lib@0.26.3-nova',
-    'nova:settings@0.26.3-nova',
-    'nova:users@0.26.3-nova',
-    'nova:posts@0.26.3-nova',
+    'nova:core@0.26.5-nova',
+    'nova:lib@0.26.5-nova',
+    'nova:settings@0.26.5-nova',
+    'nova:users@0.26.5-nova',
+    'nova:posts@0.26.5-nova',
   ]);
 
   api.mainModule("lib/server.js", "server");
